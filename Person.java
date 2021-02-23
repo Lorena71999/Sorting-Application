0f705178baf7d6a602b49bc1f1a6a87bb349f101
Maria-Lorena Comanescu <61329796+Lorena71999@users.noreply.github.com>
2021-02-23 15:31:03 +0200

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,5 @@
-  public class Person {
+ 
+   public abstract class Person {
     
        private  String id;
        private String name;
@@ -6,51 +7,53 @@
        private String ticketType;
        private boolean priority;
        private boolean specialNeed;
+       public abstract int calculateTotalPoints(int age, String ticketType,boolean priority,boolean specialNeed);
 
     public Person(String id ,String name ,int age ,String ticketType ,boolean priority , boolean specialNeed ) {
-       this.id = id;
-       this.name = name;
-       this.age = age;
-       this.ticketType = ticketType;
-       this.priority = priority;
-       this.specialNeed = specialNeed;
-   }
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.ticketType = ticketType;
+        this.priority = priority;
+        this.specialNeed = specialNeed;
+    }
 
-   public String getId(){
+    public String getId(){
         return id;
-   }
+    }
 
-   public String getName(){
+    public String getName(){
       return name;
 
-   }
+    }
 
-   public int getAge(){
+    public int getAge(){
         return age;
-   }
+    }
 
-   public boolean getPriority(){
+    public boolean getPriority(){
         return priority;
-   }
+    }
 
-   public boolean getSpecialNeed(){
+    public boolean getSpecialNeed(){
         return specialNeed;
-   }
+    }
 
-   public String getTicketType(){
+    public String getTicketType(){
         return ticketType;
-   }
+    }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() { 
         return "Person{" +
-              "id='" + id + '\'' +
-              ", name='" + name + '\'' +
-              ", age=" + age +
-              ", ticketType='" + ticketType + '\'' +
-              ", priority=" + priority +
-              ", specialNeed=" + specialNeed +
-              '}';
+                   "id='" + id + '\'' +
+                   ", name='" + name + '\'' +
+                   ", age=" + age +
+                   ", ticketType='" + ticketType + '\'' +
+                   ", priority=" + priority +
+                   ", specialNeed=" + specialNeed +
+                   '}';
     }
 
   }
+
