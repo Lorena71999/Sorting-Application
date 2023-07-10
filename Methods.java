32ada341ce4374bfcd949d68b7e290e661a4d683
Maria-Lorena Comanescu <61329796+mariaComanescu@users.noreply.github.com>
2023-07-10 18:52:26 +0300

    Delete Methods.java

--- Methods.java
+++
@@ -1,7 +0,0 @@
-public interface Methods {
-
-    int getPointsOnAge(int age);
-    int getPointsOnTicketType(String c);
-    int getPointsOnPriority(boolean boo);
-    int getPointsOnSpecialNeed(boolean boo1);
-}
